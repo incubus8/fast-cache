5989d52678e01af5e225c0d1de666c14417927bd
☁ Rheza Satria <rheza.satria.ta@gmail.com>
2018-06-13 01:18:24 +0700

    ref: add gomaxprocs

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/incubus8/fast-cache/rest"
+	"runtime"
 )
 
 func main() {
-	app := rest.NewApplication()
 
+	// Setting a higher number here allows more disk I/O calls to be scheduled, hence considerably
+	// improving throughput. The extra CPU overhead is almost negligible in comparison. The
+	// benchmark notes are located in badger-bench/randread.
+	runtime.GOMAXPROCS(128)
+
+	app := rest.NewApplication()
 	rest.NewServer(app)
 }
